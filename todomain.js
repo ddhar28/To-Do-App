5ba212ebf298ee7039422f6fb06a99b8c1b121c4
Debi Dhar <ddhar2895@gmail.com>
2019-04-11 13:36:15 -0400

    used local storage

--- todomain.js
+++ todomain.js
@@ -1,6 +1,9 @@
-let taskInp = document.getElementById('taskInp')
-let list = document.getElementById('taskList')
-let add = document.getElementById('addTask')
+const taskInp = document.getElementById('taskInp')
+const list = document.getElementById('taskList')
+const add = document.getElementById('addTask')
+const storage = window.localStorage
+let tasks = storage.length ? JSON.parse(storage.getItem('task')) : []
+let taskDetails = {}
 
 function createOption (element, className, txt, func) {
   let btn = document.createElement(element)
@@ -10,26 +13,56 @@ function createOption (element, className, txt, func) {
   return btn
 }
 
-function addItem (task) {
+function saveTask () {
+  storage.setItem('task', JSON.stringify(tasks))
+}
+
+function updateTask (id, key, value, del = false) {
+  if (!del) {
+    let storageTask = tasks.filter(temp => temp.id === id)[0]
+    tasks[tasks.indexOf(storageTask)][key] = value
+  } else tasks = tasks.filter(temp => temp.id !== id)
+  saveTask()
+}
+
+function addTask (task) {
   if (task !== '') {
-    let newTask = document.createElement('li')
-    let taskName = document.createElement('input')
-    taskName.value = task
-    taskName.setAttribute('class', 'noEdit')
-    taskName.addEventListener('keydown', (e) => {
-      taskName.setAttribute('class', 'editMode')
-      enter(e, editTask)
-    })
-    taskName.disabled = true
-    newTask.appendChild(taskName)
-    newTask.append(createOption('button', 'delete', '\u2718', (e) => e.target.parentNode.parentNode.removeChild(e.target.parentNode)))
-    newTask.append(createOption('button', 'complete', '\u2714', completeTask))
-    newTask.append(createOption('button', 'edit', '\u270D', editTask))
-    newTask.append(createOption('button', 'addNote', '\u271A Note', editNote))
-    setTimeout(() => (newTask.setAttribute('class', 'active')), 10)
-    if (list.children.length < 1) list.appendChild(newTask)
-    else list.insertBefore(newTask, list.childNodes[0])
+    taskDetails = { 'id': Math.floor(Math.random() * 100) + task.slice(0, 5), 'title': task, 'class': 'active', 'note': '' }
+    tasks.push(taskDetails)
+    displayTask(taskDetails)
+    saveTask()
   }
+}
+
+function displayTask (task) {
+  let newTask = document.createElement('li')
+  let taskName = document.createElement('input')
+  taskName.value = task.title
+  taskName.setAttribute('class', 'noEdit')
+  taskName.addEventListener('keydown', (e) => {
+    taskName.setAttribute('class', 'editMode')
+    enter(e, editTask)
+  })
+  taskName.disabled = true
+  newTask.appendChild(taskName)
+  newTask.append(createOption('button', 'delete', '\u2718', (e) => {
+    updateTask(task.id, '', '', true)
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode)
+  }))
+  newTask.append(createOption('button', 'complete', task.class === 'active' ? '\u2714' : '\u27F3', completeTask))
+  let isNote = task.note !== ''
+  newTask.append(createOption('button', 'addNote', !isNote ? '\u271A Note' : String.fromCodePoint(128065) + ' Note', editNote))
+  if (isNote) {
+    let taskNote = document.createElement('textarea')
+    taskNote.setAttribute('class', 'noteDone')
+    taskNote.value = task.note
+    newTask.appendChild(taskNote)
+  }
+  newTask.append(createOption('button', 'edit', '\u270D', editTask))
+  setTimeout(() => (newTask.setAttribute('class', task.class)), 10)
+  newTask.setAttribute('id', task.id)
+  if (list.children.length < 1) list.appendChild(newTask)
+  else list.insertBefore(newTask, list.childNodes[0])
   taskInp.value = ''
   taskInp.focus()
 }
@@ -42,6 +75,7 @@ function completeTask (e) {
   task.querySelector('.edit').disabled = isActive
   task.querySelector('.addNote').disabled = isActive
   e.target.textContent = isActive ? '\u27F3' : '\u2714'
+  updateTask(task.getAttribute('id'), 'class', taskClass)
   if (isActive) list.insertBefore(task, list.lastChild.nextSibling)
   else list.insertBefore(task, list.firstChild)
   taskInp.focus()
@@ -58,6 +92,7 @@ function editTask (e) {
   if (isEdit) taskInp.focus()
   else editBox.focus()
   if (editBox.value === '') editBox.value = 'Untitled'
+  updateTask(editBox.parentNode.getAttribute('id'), 'title', editBox.value)
 }
 
 function editNote (e) {
@@ -70,7 +105,8 @@ function editNote (e) {
   noteBox.setAttribute('class', isEditable ? 'noteDone' : 'noteEdit')
   noteBox.parentNode.querySelector('.complete').disabled = !isEditable
   noteBox.parentNode.querySelector('.delete').disabled = !isEditable
-  e.target.parentNode.style.height = isEditable ? 'auto' : '150px'
+  e.target.parentNode.style.height = isEditable ? '30px' : '150px'
+  updateTask(noteBox.parentNode.getAttribute('id'), 'note', noteBox.value)
   let noVal = noteBox.value.trim() === ''
   if (isEditable && noVal) noteBox.parentNode.removeChild(noteBox)
   e.target.textContent = (isEditable ? (noVal ? '\u271A Note' : String.fromCodePoint(128065) + ' Note') : 'Done')
@@ -78,10 +114,24 @@ function editNote (e) {
 }
 
 function enter (e, f, para = e) {
-  if (e.code === 'Enter') f(para)
+  if (e.code === 'Enter') {
+    f(para)
+  }
 }
 
 taskInp.value = ''
-add.addEventListener('click', (e) => addItem(taskInp.value.trim()))
-taskInp.addEventListener('keydown', (e) => enter(e, addItem, taskInp.value.trim()))
+add.addEventListener('click', (e) => {
+  addTask(taskInp.value.trim())
+})
+
+taskInp.addEventListener('keydown', (e) => {
+  enter(e, addTask, taskInp.value.trim())
+})
+
 taskInp.focus()
+if (storage.length) {
+  const data = JSON.parse(storage.getItem('task'))
+  data.forEach(item => {
+    displayTask(item)
+  })
+}
